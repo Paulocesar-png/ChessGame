1cbb5825c2e376fe3e7b332480eb7331e54d8403
Paulo Cesar <cesar.pc942@gmail.com>
2022-12-01 09:21:53 -0300

    Creating a new class PosicaoXadrez

---
+++ ChessGame/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using ChessGame.tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez() {
+
+        }
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString() {
+            return "" + linha +  coluna;
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+    }
+}
