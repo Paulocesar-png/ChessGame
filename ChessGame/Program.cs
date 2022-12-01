eacaf4bc65bb0c190821e48bbc50def75e2849b1
Paulo Cesar <cesar.pc942@gmail.com>
2022-12-01 09:15:18 -0300

    Implementation of a custom exception

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -8,6 +8,8 @@ namespace ChessGame
     class Program
     {
         static void Main(string[] args) {
+            try {
+
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
@@ -15,6 +17,9 @@ namespace ChessGame
             tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
+            } catch (TabuleiroExecption e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
